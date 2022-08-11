0dd424c3ef01c828da931d2fab6509b842c682c5
Bunnasorn Kaewsiri <bunnasorn.k@ku.th>
2022-08-11 20:29:37 +0700

    Change to mongodb

--- database/db.js
+++ database/db.js
@@ -1,7 +1,38 @@
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
+// const low = require("lowdb");
+// const FileSync = require("lowdb/adapters/FileSync");
 
-const adapter = new FileSync("./database/db.json");
-const db = low(adapter);
+// const adapter = new FileSync("./database/db.json");
+// const db = low(adapter);
 
-module.exports = { db };
+// module.exports = { db };
+
+const mongoose = require("mongoose");
+
+const DB_URL =
+  "mongodb+srv://3xbun-user:top2541top@test0.hqdnz.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "MongoDB connection error: ")
+);
+
+const AnswersSchema = new mongoose.Schema({
+  q1: { type: String, required: true },
+  q2: { type: String, required: true },
+  q3: { type: String, required: true },
+  q4: { type: String, required: true },
+  q5: { type: String, required: true },
+  q6: { type: String, required: true },
+  q7: { type: String, required: true },
+  q8: { type: String, required: true },
+  q9: { type: String, required: true },
+  q10: { type: String, required: true },
+  q11: { type: String, required: true },
+  q12: { type: String, required: true },
+  datetime: { type: Date, default: Date.now },
+});
+
+const Answers = mongoose.model("Answers", AnswersSchema);
+
+module.exports = { Answers };
--- routes/Index.js
+++ routes/Index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { db } = require("../database/db");
+const { Answers } = require("../database/db");
 
 const router = express.Router();
 
@@ -8,9 +8,9 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
-router.get("/answers", (req, res) => {
-  const answers = db.get("answers").value();
-  res.json(answers);
+router.get("/answers", async (req, res) => {
+  const ans = await Answers.find();
+  return res.status(200).json(ans);
 });
 
 router.get("/admin", (req, res) => {
@@ -18,12 +18,24 @@ router.get("/admin", (req, res) => {
 });
 
 // Add answer
-router.post("/", (req, res) => {
-  const answer = req.body;
+router.post("/", async (req, res) => {
+  const answer = new Answers({
+    q1: req.body.q1,
+    q2: req.body.q2,
+    q3: req.body.q3,
+    q4: req.body.q4,
+    q5: req.body.q5,
+    q6: req.body.q6,
+    q7: req.body.q7,
+    q8: req.body.q8,
+    q9: req.body.q9,
+    q10: req.body.q10,
+    q11: req.body.q11,
+    q12: req.body.q12,
+  });
 
-  db.get("answers").push(answer).last().write();
-
-  res.json(answer);
+  await answer.save();
+  return res.status(201).json(req.body);
 });
 
 module.exports = router;
