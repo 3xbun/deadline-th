04f738f9bd3ed0f67da9038e4e7e7f4cfc2c3310
Bunnasorn Kaewsiri <bunnasorn.k@ku.th>
2022-08-11 20:51:18 +0700

    Connect front to back

--- routes/Index.js
+++ routes/Index.js
@@ -8,17 +8,26 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
-router.get("/answers", async (req, res) => {
-  const ans = await Answers.find();
-  return res.status(200).json(ans);
-});
-
 router.get("/admin", (req, res) => {
   res.render("admin");
 });
 
+// API //
+
+// Get all answers
+router.get("/api/answers", async (req, res) => {
+  const ans = await Answers.find();
+  return res.status(200).json(ans);
+});
+
 // Add answer
-router.post("/", async (req, res) => {
+router.post("/api/answers", async (req, res) => {
+  Object.values(req.body).every((item) => {
+    if (item === "") {
+      return res.status(400).json({ msg: "Answer can't be empty" });
+    }
+  });
+
   const answer = new Answers({
     q1: req.body.q1,
     q2: req.body.q2,
@@ -35,7 +44,13 @@ router.post("/", async (req, res) => {
   });
 
   await answer.save();
-  return res.status(201).json(req.body);
+  return res.status(201);
+});
+
+router.delete("/api/answers/:id", async (req, res) => {
+  const { id } = req.params;
+  await Answers.findByIdAndDelete(id);
+  return res.status(200).json({ msg: "Delete successfully" });
 });
 
 module.exports = router;
